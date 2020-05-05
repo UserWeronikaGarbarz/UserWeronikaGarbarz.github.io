7aaac048764b6f0508bc776ab7517af1f76133c9
UserWeronikaGarbarz <weronika.garbarz@gmail.com>
2020-05-06 01:03:20 +0200

    1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://immense-anchorage-79940.herokuapp.com/v1/';
-  const trelloApiRoot = 'https://immense-anchorage-79940.herokuapp.com/v1/trello/boards';
+  const trelloApiRoot = 'https://immense-anchorage-79940.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
